65a58fab28507000fe0295f9416216b901395969
JoshuaHoffmann <JoshuaHoffmann@protonmail.com>
2020-02-03 20:39:31 +0100

    - Split Js functions into seperate files - Basic styling - Added function for editing paragraphs

---
+++ js/edit_p.js
@@ -0,0 +1,3 @@
+function edit_p(id, str) {
+     document.getElementById(id).innerHTML = str;
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+var numerator_1;
+var denominator_1;
+
+var operation;
+
+var numerator_2;
+var denominator_2;
 
