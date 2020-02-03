bcf8a0591d3f017b6297882d2a1fb0727eecc6e4
daniel-201 <60601872+daniel-201@users.noreply.github.com>
2020-02-03 18:19:33 +0100

    added kgV and ggT

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-var loool;
+function ggT(a, b) {
+  var r =  a % b;
+  while(r > 0) {
+    a = b;
+    b = r;
+    r = a % b;
+  }
+  return b;
+}
+
+function kgV(a, b) {
+  var v = b;
+  while(v % a != 0) {
+    v += b;
+  }
+  return v;
+}
